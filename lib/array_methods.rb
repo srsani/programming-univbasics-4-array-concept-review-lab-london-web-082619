33f8fbc6fc54b66cb442696d1156c6b47e178ef7
droll-accelerator-1483 <srsani@gmail.com>
2019-07-21 13:56:19 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -24,5 +24,12 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+    start_a =array[0] 
+  for i in array
+    if i < start_a
+      start_a = i
+    end
+  end
+  return start_a
+end
 end
