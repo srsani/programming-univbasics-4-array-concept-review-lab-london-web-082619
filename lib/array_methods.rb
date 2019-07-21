d663050bad30696e40d165e7e82b02b59e25248a
droll-accelerator-1483 <srsani@gmail.com>
2019-07-21 13:54:59 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -14,13 +14,12 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-
   for i in [1,2,1,3,4,3,5,4,3,2,1]
     if i > start_a
       start_a = i
     end
-  end 
-
+  end
+  return start_a
 end
 
 def find_min_value(array)
